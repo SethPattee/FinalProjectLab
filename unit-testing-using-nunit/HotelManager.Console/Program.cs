34f081e096207d489dd875ce7d57497b9f055275
SethPattee <97485847+SethPattee@users.noreply.github.com>
2022-05-02 09:21:02 -0600

    Final Changes

--- unit-testing-using-nunit/HotelManager.Console/Program.cs
+++ unit-testing-using-nunit/HotelManager.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ConsoleTables;
 namespace HotelManager;
 //this is the UI for the program
 public class Program
@@ -10,35 +11,66 @@ public class Program
         var checkInList = new List<CheckIn>();
         contactList.Add(new Contact("Dick", "Grayson", "1227 Gothem street", "Gothem", "123-123-1234"));
         checkInList.Add(new CheckIn(DateTime.Now, CheckInStatus.CheckedIn));
+
+
+
+
+
         while (true)
         {
-            Console.WriteLine("Please choose an option to explore!\n0: Display Info\n1: Add Person\n2: Remove Person\n3: Save People\n4: Load From Save\n5: Change Status\n6: Exit");
+            //Console.WriteLine("Please choose an option to explore!\n0: Display Info\n1: Add Person\n2: Remove Person\n3: Save People\n4: Load From Save\n5: Change Status\n6: Exit");
+            Console.Clear();
+            Console.WriteLine("Please choose an option to explore!");
+            var Table = new ConsoleTable("Number to Enter", "Name of Command", "Discription");
+            Table.AddRow("0", "Display Info", "Displays the info of the people that you have saved");
+            Table.AddRow("1", "Add Person", "Lets you add and save a person's info");
+            Table.AddRow("2", "Remove Person", "Lets you delete a person and their info");
+            Table.AddRow("3", "Save People", "Lets you save people to a text file for later use or to print out");
+            Table.AddRow("4", "Load From Save", "If you already have people saved in a text file, it wiil add them to your current list");
+            Table.AddRow("5", "Change Status", "Lets you change the status of a person");
+            Table.AddRow("6", "Exit", "Quits the program");
+            Table.Write(Format.Alternative);
+            Console.WriteLine("Enter the number of the command that you would like to run");
+
             var option = Console.ReadLine();
             switch (option)
             {
                 case "0":
                     Console.Clear();
                     ControlMethods.DisplayInfo(contactList, checkInList);
-                    Console.WriteLine("Done");
+                    Console.WriteLine("Back to the main menu...");
+                    Thread.Sleep(1000);
                     break;
                 case "1":
                     ControlMethods.AddPerson(contactList, checkInList);
+                    Console.WriteLine("Person has been saved!");
+                    Console.WriteLine("Back to the main menu...");
+                    Thread.Sleep(1000);
                     break;
                 case "2":
                     ControlMethods.RemovePerson(contactList, checkInList);
-                    Console.WriteLine("Person has been removed");
+                    Console.WriteLine("Back to the main menu...");
+                    Thread.Sleep(1000);
                     break;
                 case "3":
                     ControlMethods.WriteToFile(contactList);
+                    Console.Clear();
                     Console.WriteLine("Contacts Saved!");
+                    Console.WriteLine("Back to the main menu...");
+                    Thread.Sleep(1500);
                     break;
                 case "4":
                     ControlMethods.WriteToList(contactList, checkInList);
+                    Console.Clear();
                     Console.WriteLine("Contacts Loaded!");
+                    Console.WriteLine("Back to the main menu...");
+                    Thread.Sleep(1500);
                     break;
                 case "5":
                     ControlMethods.ChangeStatus(contactList, checkInList);
                     Console.WriteLine("Status Changed!");
+                    Console.WriteLine("Back to the main menu...");
+                    Thread.Sleep(1000);
                     break;
                 case "6":
                     return;
--- unit-testing-using-nunit/HotelManager/HotelManager.cs
+++ unit-testing-using-nunit/HotelManager/HotelManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ConsoleTables;
 using System.Collections.Generic;
 using System.Diagnostics;
 namespace HotelManager;
@@ -10,13 +11,22 @@ public class ControlMethods
     {
         int counter = 0;
         Console.WriteLine("Displaying info");
+        var table = new ConsoleTable("  Number  ", "  First Name  ", "  Last Name  ", "  Street Address  ", "  City, State, Zip Code  ", "  Phone Number  ", "  Status  ", "  Last Updated  ");
 
         foreach (var person in contactList)
         {
-            Console.WriteLine($"{counter}:{Contact.GetInfo(person)} Status: {checkinList[counter].Status.ToString()}   Last Updated: {checkinList[counter].LastUpdate.ToString("MM/dd/yyyy hh:mm tt")}");
+            
+            table.AddRow(counter, person.FirstName, person.LastName, person.StreetAddress, person.CityStateZip, person.Phone, checkinList[counter].Status.ToString(), checkinList[counter].LastUpdate.ToString("MM/dd/yyyy hh:mm tt"));
+            //table.Write(Format.Alternative);
+            Console.WriteLine();
+            //Console.WriteLine($"{counter}:{Contact.GetInfo(person)} Status: {checkinList[counter].Status.ToString()}   Last Updated: {checkinList[counter].LastUpdate.ToString("MM/dd/yyyy hh:mm tt")}");
             counter++;
         }
-        return "Done";
+        table.Write(Format.Alternative);
+        Console.WriteLine("Done. Press enter to go back to the Main Menu");
+        Console.ReadKey();
+        return "Back to the main menu...";
+
     }
     public static void AddPerson(List<Contact> contactList, List<CheckIn> checkinList)
     {
@@ -26,12 +36,11 @@ public class ControlMethods
             try
             {
                 Console.Clear();
-                Console.WriteLine("No commas allowed!!!");
                 Console.WriteLine("Please enter the first name");
                 string firstName = Console.ReadLine()!;
                 Console.WriteLine(firstName);
 
-                Console.WriteLine("No commas allowed!!!");
+
                 Console.WriteLine("Please enter the last name");
                 string lastName = Console.ReadLine()!;
                 Console.WriteLine(lastName);
@@ -46,7 +55,6 @@ public class ControlMethods
                 string citystatezip = Console.ReadLine()!;
                 Console.WriteLine(citystatezip);
 
-                Console.WriteLine("No commas allowed!!!");
                 Console.WriteLine("Please enter the phone number");
                 string phone = Console.ReadLine()!;
                 Console.WriteLine(phone);
@@ -54,15 +62,13 @@ public class ControlMethods
                 // this is to make sure that there are no commas that will mess up the saving. 
                 char value = ',';
                 bool result = checkComma.Contains(value);
-                Console.WriteLine(checkComma);
-                //Add excption here?
-
+                Console.Clear();
                 Console.WriteLine($"Is this correct: {firstName} {lastName}, {streetaddress}, {citystatezip}, {phone}?\ny/n");
                 var yesno = Console.ReadLine();
                 if (result)
                 {
 
-                    Console.WriteLine("Try again. You have a comma somwhere. Enter anything but 'y' or 'n' to quit");
+                    Console.WriteLine("Try again. You have a comma somwhere. Press enter to quit");
                     Console.WriteLine($"Is this correct:{firstName} {lastName}, {streetaddress}, {citystatezip}, {phone}?\ny/n");
                     yesno = Console.ReadLine();
                     if (yesno != "y" && yesno != "n")//for quitting
@@ -99,6 +105,7 @@ public class ControlMethods
     }
     public static void RemovePerson(List<Contact> contactList, List<CheckIn> checkInList)
     {
+        Console.Clear();
         int input = 0;
 
         bool badInput = true;
@@ -117,13 +124,18 @@ public class ControlMethods
                 Console.WriteLine("No people stored");
                 return;
             }
-            Console.WriteLine($"There are {counter} people stored.\nPlease choose the person the you would like to delete");
+            Console.WriteLine($"There are {counter} people stored.\nPlease enter the number of the person that you would like to delete\nPress enter to quit");
+            var table = new ConsoleTable("  Number  ", "  First Name  ", "  Last Name  ", "  Street Address  ", "  City, State, Zip Code  ", "  Phone Number  ");
             foreach (var person in contactList)
             {
-                Console.WriteLine($"{counter2}: {person.FirstName}");
+                
+                table.AddRow(counter2, person.FirstName, person.LastName, person.StreetAddress, person.CityStateZip, person.Phone);
+                Console.WriteLine();
+                //Console.WriteLine($"{counter}:{Contact.GetInfo(person)}");
                 counter2++;
             }
-            //TODO Add a way to quit
+            table.Write(Format.Alternative);
+
 
             try
             {
@@ -134,19 +146,19 @@ public class ControlMethods
                 }
                 else
                 {
+                    contactList.Remove(contactList[input - 1]);
+                    checkInList.Remove(checkInList[input - 1]);
+                    Console.WriteLine("Person has been removed!");
                     badInput = false;
                 }
             }
             catch
             {
-                Console.WriteLine("You entered a letter. Now you have to restart. Next time, please enter a number");
-                Console.ReadLine();
-                return;
-                throw new YouEnteredALetter("You entered a letter. Now you have to restart. Next time, please enter a number");
+                badInput = false;
+                //throw new YouEnteredALetter("You entered a letter. Now you have to restart. Next time, please enter a number");
             }
         }
-        contactList.Remove(contactList[input - 1]);
-        checkInList.Remove(checkInList[input - 1]);
+
     }
     public static void WriteToFile(List<Contact> contactList)
     {
@@ -210,12 +222,17 @@ public class ControlMethods
                 Console.WriteLine("No people stored");
                 return;
             }
-            Console.WriteLine($"There are {counter} people stored.\nPlease enter the number of the person who's status would you like to change.");
+            Console.WriteLine($"There are {counter} people stored.\nPlease enter the number of the person who's status would you like to change.\n Press enter to quit");
+            var table = new ConsoleTable("  Number  ", "  First Name  ", "  Last Name  ", "  Street Address  ", "  City, State, Zip Code  ", "  Phone Number  ");
             foreach (var person in contactList)
             {
-                Console.WriteLine($"{counter2}: {person.FirstName}");
+                
+                table.AddRow(counter2, person.FirstName, person.LastName, person.StreetAddress, person.CityStateZip, person.Phone);
+                Console.WriteLine();
+                //Console.WriteLine($"{counter}:{Contact.GetInfo(person)}");
                 counter2++;
             }
+            table.Write(Format.Alternative);
             //TODO Add a way to quit
 
             try
@@ -234,8 +251,8 @@ public class ControlMethods
             {
 
                 Console.ReadLine();
-                return;
-                throw new YouEnteredALetter("You entered a letter. Now you have to restart. Next time, please enter a number");
+                badInput = false;
+                //throw new YouEnteredALetter("You entered a letter. Now you have to restart. Next time, please enter a number");
 
             }
         }
@@ -258,7 +275,7 @@ public class ControlMethods
                     Console.WriteLine("Done");
                     return;
                 case "2":
-                    CheckInStatus notcheckedin = CheckInStatus.CheckedIn;
+                    CheckInStatus notcheckedin = CheckInStatus.NotCheckedIn;
                     checkInList[input - 1].status = notcheckedin;
                     checkInList[input - 1].LastUpdate = DateTime.Now;
                     Console.WriteLine("Done");
